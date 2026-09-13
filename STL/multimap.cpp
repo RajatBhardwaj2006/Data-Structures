ff0428702d94061a37c259bc844fff834c34aa00
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-09-13 23:02:14 +0530

    MultiMap - Standard Template library (STL)

---
+++ STL/multimap.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void explain_multimap(){
+    multimap <int, int> mm;
+    // Everything as same as map, only it can store multiple keys
+    // only m[key] cannot be used here
+    
+}
+void explain_unorder_map(){
+    unordered_map <int, int> mm;
+    // same as set and unorered_set differnce
+    
+}
+int main(){
+    explain_multimap();
+
+    explain_unorder_map();
+    return 0;
+}
