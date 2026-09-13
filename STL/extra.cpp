96016d79bea4163635e94e3efd60040d0b7efd2a
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-09-13 23:25:13 +0530

    Some extra STL Function: Sort, PopCount, and more

--- STL/extra.cpp
+++ STL/extra.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool comp(pair <int, int> p1, pair<int,int> p2){
+    if(p1.second < p2.second){
+        return true;
+    }
+    if(p1.second > p2.second){
+        return false;
+    }
+    if(p1.first > p2.first){
+        return true;
+    }
+    return false;
+}
 void explain_sort(){
     //! Sort  
     vector <int> v;
@@ -22,6 +35,14 @@ void explain_sort(){
     for(auto x : v){
         cout << x << " ";
     }
+}
+
+void explain_popcount(){
+    int num1 = 7;
+    int cnt = __builtin_popcount(num1);
+
+    long long num2 = 123234335454;
+    int cnt = __builtin_popcountll(num2);
 }
 int main(){
     explain_sort();
